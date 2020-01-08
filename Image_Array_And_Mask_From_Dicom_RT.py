692db47dda5c19943120e83bf1b9839f0345a9f3
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-01-08 16:48:14 -0600

    removed plot_scroll_Images from within function, ref module now

--- Image_Array_And_Mask_From_Dicom_RT.py
+++ Image_Array_And_Mask_From_Dicom_RT.py
@@ -4,60 +4,7 @@ from pydicom.tag import Tag
 import SimpleITK as sitk
 from skimage import draw
 from scipy.ndimage.morphology import binary_fill_holes
-import matplotlib.pyplot as plt
 from skimage.measure import label,regionprops,find_contours
-
-
-def plot_scroll_Image(x):
-    '''
-    :param x: input to view of form [rows, columns, # images]
-    :return:
-    '''
-    if x.dtype not in ['float32','float64']:
-        x = copy.deepcopy(x).astype('float32')
-    if len(x.shape) > 3:
-        x = np.squeeze(x)
-    if len(x.shape) == 3:
-        if x.shape[0] != x.shape[1]:
-            x = np.transpose(x,[1,2,0])
-        elif x.shape[0] == x.shape[2]:
-            x = np.transpose(x, [1, 2, 0])
-    fig, ax = plt.subplots(1, 1)
-    if len(x.shape) == 2:
-        x = np.expand_dims(x,axis=0)
-    tracker = IndexTracker(ax, x)
-    fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
-    return fig,tracker
-
-
-class IndexTracker(object):
-    def __init__(self, ax, X):
-        self.ax = ax
-        ax.set_title('use scroll wheel to navigate images')
-
-        self.X = X
-        rows, cols, self.slices = X.shape
-        self.ind = np.where(self.X != 0)[-1]
-        if len(self.ind) > 0:
-            self.ind = self.ind[len(self.ind)//2]
-        else:
-            self.ind = self.slices//2
-
-        self.im = ax.imshow(self.X[:, :, self.ind],cmap='gray')
-        self.update()
-
-    def onscroll(self, event):
-        print("%s %s" % (event.button, event.step))
-        if event.button == 'up':
-            self.ind = (self.ind + 1) % self.slices
-        else:
-            self.ind = (self.ind - 1) % self.slices
-        self.update()
-
-    def update(self):
-        self.im.set_data(self.X[:, :, self.ind])
-        self.ax.set_ylabel('slice %s' % self.ind)
-        self.im.axes.figure.canvas.draw()
 
 
 class Dicom_to_Imagestack:
