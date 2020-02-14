9db013e4490b85f1b0241b1841b06b4c83e65ccb
Anderson <BMAnderson@mdanderson.org>
2020-02-14 10:11:17 -0600

    update template_dir to find if placed within submodules

--- Image_Array_And_Mask_From_Dicom_RT.py
+++ Image_Array_And_Mask_From_Dicom_RT.py
@@ -20,7 +20,8 @@ class Dicom_to_Imagestack:
         self.arg_max = arg_max
         self.rewrite_RT_file = rewrite_RT_file
         if template_dir is None:
-            template_dir = os.path.join(__file__[:__file__.index(__package__)], __package__, 'template_RS.dcm')
+            package_name = __package__.split('.')[-1]
+            template_dir = os.path.join(__file__[:__file__.index(package_name)],package_name,'template_RS.dcm')
         self.template_dir = template_dir
         self.template = True
         self.delete_previous_rois = delete_previous_rois
