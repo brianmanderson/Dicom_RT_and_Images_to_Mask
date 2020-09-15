e4ab32f43a3bbfa330ff400bdd10efbb13b43294
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-15 14:22:28 -0500

    adding classifiers and and long_descriptions

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ __author__ = 'Brian M Anderson'
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='DicomRTTools',
     author='Brian Mark Anderson',
@@ -11,6 +14,12 @@ setup(
     version='0.0.2',
     description='Tools for reading dicom files, RT structures, and dose files, as well as tools for '
                 'converting numpy prediction masks back to an RT structure',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=['DicomRTTool'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3"
+    ]
 )
