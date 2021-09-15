f73792898d2e8a6ed464c2f6b01eb594b3cfe59f
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-09-15 14:58:21 -0700

    modularizing things

--- src/Distribute_Train_Test_Validation.py
+++ src/Distribute_Train_Test_Validation.py
@@ -7,9 +7,90 @@ import typing
 import pandas as pd
 
 
-def distribute(niftii_path: typing.Union[str, bytes, os.PathLike],  excel_file: typing.Union[str, bytes, os.PathLike],
-               validation_fraction: float, test_fraction=0):
+def define_folders_for_excel_sheet(excel_file: typing.Union[str, bytes, os.PathLike],
+                                   validation_fraction: float, test_fraction=0,
+                                   patientIDcolumnname='PatientID') -> pd.DataFrame:
     """
+    The goal of this is to provide a Folder name (Train, Test, or Validation) for each image set based
+    on the patient ID
+    :param patientIDcolumnname: string of the column name that you want to distribute across
+    :param excel_file: the excel file which was created during the niftii writing process
+    :param validation_fraction: fraction [0-1] of data to be placed in the validation set
+    :param test_fraction: fraction [0-1] of the data to be placed in the test set
+    :return: a pandas dataframe of the loaded excel sheet
+    """
+    assert os.path.exists(excel_file), FileExistsError("File not found")
+    data_df = pd.read_excel(excel_file, engine='openpyxl')
+
+    unique_patients = np.unique(data_df[patientIDcolumnname].values)
+    total_patients = len(unique_patients)
+    patient_image_dictionary = {}
+    not_distributed = []
+    validation_mrns = []
+    test_mrns = []
+    for patient_MRN in unique_patients:
+        patient_indexes = data_df.loc[data_df[patientIDcolumnname] == patient_MRN].index.values
+        patient_image_dictionary[patient_MRN] = {'Indexes': patient_indexes, 'Folder': None}
+        """
+        Check to see if we have already assigned a Train/Test/Validation folder to this patient's MRN
+        We do not want to break up a patient's images!
+        """
+        folder = None
+        for patient_folder in data_df.Folder[patient_indexes]:
+            if patient_folder == 'Train':
+                folder = 'Train'
+                break
+            elif patient_folder == 'Validation':
+                folder = 'Validation'
+                validation_mrns.append(patient_MRN)
+                break
+            elif patient_folder == 'Test':
+                folder = 'Test'
+                test_mrns.append(patient_MRN)
+                break
+        patient_image_dictionary[patient_MRN]['Folder'] = folder
+        """
+        If this patient was already assigned a folder, assign that folder to the other images and save
+        """
+        if folder is not None:
+            for index in patient_indexes:
+                data_df.Folder[index] = folder
+            data_df.to_excel(excel_file, index=0)
+        else:
+            not_distributed.append(patient_MRN)
+    not_distributed = np.asarray(not_distributed)
+    if len(not_distributed) > 0:
+        """
+        Shuffle the indexes up that haven't been distributed
+        """
+        perm = np.arange(len(not_distributed))
+        np.random.shuffle(perm)
+        not_distributed = not_distributed[perm]
+        """
+        For each patient, check to see if they should go in validation or test, overflow goes into train
+        """
+        for patient_MRN in not_distributed:
+            patient_indexes = patient_image_dictionary[patient_MRN]['Indexes']
+            number_to_validation = int(total_patients * validation_fraction) - len(validation_mrns)
+            number_to_test = int(total_patients * test_fraction) - len(test_mrns)
+            folder = 'Train'
+            if number_to_validation > 0:
+                folder = 'Validation'
+                validation_mrns.append(patient_MRN)
+            elif number_to_test > 0:
+                folder = 'Test'
+                test_mrns.append(patient_MRN)
+            for index in patient_indexes:
+                data_df['Folder'][index] = folder
+        data_df.to_excel(excel_file, index=0)
+    return data_df
+
+
+def distribute(niftii_path: typing.Union[str, bytes, os.PathLike],
+               excel_file: typing.Union[str, bytes, os.PathLike],
+               validation_fraction: float, test_fraction=0, patientIDcolumnname='PatientID'):
+    """
+    :param patientIDcolumnname: string of the column name that you want to distribute across
     :param niftii_path: path to the niftii files
     :param excel_file: the excel file which was created during the niftii writing process
     :param validation_fraction: fraction [0-1] of data to be placed in the validation set
@@ -23,81 +104,16 @@ def distribute(niftii_path: typing.Union[str, bytes, os.PathLike],  excel_file: 
         if not os.path.exists(out_path):
             os.makedirs(out_path)
 
-    assert os.path.exists(excel_file), FileExistsError("File not found")
-    data_df = pd.read_excel(excel_file, engine='openpyxl')
-
-    total_patients = np.unique(data_df.PatientID.values)
-    patient_image_dictionary = {}
-    for patient_MRN in total_patients:
-        patient_indexes = data_df.loc[data_df.PatientID == patient_MRN].index.values
-        patient_image_dictionary[patient_MRN] = {'Indexes': patient_indexes, 'Folder': None}
-
-        """
-        Check to see if we have already assigned a Train/Test/Validation folder to this patient's MRN
-        We do not want to break up a patient's images!
-        """
-        folder = None
-        for patient_folder in data_df.Folder[patient_indexes]:
-            if patient_folder == 'Train':
-                folder = 'Train'
-                break
-            elif patient_folder == 'Validation':
-                folder = 'Validation'
-                break
-            elif patient_folder == 'Test':
-                folder = 'Test'
-                break
-        patient_image_dictionary[patient_MRN]['Folder'] = folder
-        """
-        If this patient was already assigned a folder, assign that folder to the other images and save
-        """
-        if folder is not None:
-            for index in patient_indexes:
-                data_df.Folder[index] = folder
-            data_df.to_excel(excel_file, index=0)
-        xxx = 1
-    not_distributed = data_df.loc[pd.isnull(data_df.Folder)]
-    indexes_not_distributed = not_distributed.index.values
-    validation_indexes = data_df.loc[data_df.Folder == 'Validation'].index.values
-    test_indexes = data_df.loc[data_df.Folder == 'Test'].index.values
-
-    if len(indexes_not_distributed) > 0:
-        """
-        Shuffle the indexes up that haven't been distributed
-        """
-        perm = np.arange(len(indexes_not_distributed))
-        np.random.shuffle(perm)
-        indexes_not_distributed = indexes_not_distributed[perm]
-        number_to_validation = int(total_patients * validation_fraction) - len(validation_indexes)
-        number_to_test = int(total_patients * test_fraction) - len(test_indexes)
-        for index in indexes_not_distributed:
-            if number_to_validation > 0:
-                data_df['Folder'][index] = 'Validation'
-                number_to_validation -= 1
-            elif number_to_test > 0:
-                data_df['Folder'][index] = 'Test'
-                number_to_test -= 1
-            else:
-                data_df['Folder'][index] = 'Train'
-        data_df.to_excel(excel_file, index=0)
+    data_df = define_folders_for_excel_sheet(excel_file=excel_file, patientIDcolumnname=patientIDcolumnname,
+                                             validation_fraction=validation_fraction, test_fraction=test_fraction)
 
     file_list = [i for i in os.listdir(niftii_path) if i.find('Overall_Data') == 0]
     '''
     Group all of the images up based on their MRN, we don't want to contaminate other groups
     '''
-
-    image_dict = dict()
-    file_dict = dict()
     for file in file_list:
         iteration = file.split('_')[-1].split('.')[0]
-        file_dict[iteration] = file
-        index = final_out_dict['Iteration'].index(iteration)
-        patient_id = final_out_dict['MRN'][index]
-        if patient_id not in image_dict:
-            image_dict[patient_id] = [iteration]
-        else:
-            image_dict[patient_id].append(iteration)
-
+        needed_folder = data_df.Folder[data_df.Iteration == iteration].index.values
     patient_image_keys = list(image_dict.keys())
     perm = np.arange(len(patient_image_keys))
     np.random.shuffle(perm)
