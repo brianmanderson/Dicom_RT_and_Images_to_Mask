e0a2f44c8410b645b5fdf7ca548e5297a3af6a02
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-09-29 17:35:29 -0700

    update

--- Test.py
+++
@@ -1,6 +0,0 @@
-from src.DicomRTTool.ReaderWriter import DicomReaderWriter, plot_scroll_Image
-
-
-path = r'C:\Users\b5anderson\Desktop\Modular_Projects\Dicom_RT_and_Images_to_Mask\Examples\Example_Data\Image_Data'
-reader = DicomReaderWriter(description='Examples', arg_max=True)
-reader.walk_through_folders(path)
