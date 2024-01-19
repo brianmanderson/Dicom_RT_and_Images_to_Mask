dc5a17e69302f94272e8aeacf0ebbcb5bcce7dd8
Max You <JianzhongMax.You@ibm.com>
2024-01-19 13:25:29 -0500

    progress, pending to code for stratify the nii and mask.nii files

--- build_flair_dicom_struct.py
+++ build_flair_dicom_struct.py
@@ -36,7 +36,6 @@ def get_roi_contour_ds(rt_sequence, index):
     contours = [contour for contour in ROI.ContourSequence]
     return contours
 
-
 def get_contour_file(path):
     """
     Get contour file from a given path by searching for ROIContourSequence 
@@ -110,40 +109,108 @@ def get_corresponding_img_ids(contour_datasets, path):
 
     return new_ReferencedSOPInstanceUID, dcm_slice.SeriesInstanceUID
 
-PATIENT_NAME = 'GBM149'
-DAY_FOLDER_FLAIR_NAME = 'Day0FLAIR'
-DAY_FOLDER_T1C_NAME = 'Day0T1c'
 
-patient_day_dicoms_T1c = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/{}/{}'.format(PATIENT_NAME, DAY_FOLDER_T1C_NAME)
-patient_day_dicoms_Flair = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/{}/{}'.format(PATIENT_NAME, DAY_FOLDER_FLAIR_NAME)
-contours_file = get_contour_file(patient_day_dicoms_T1c)
-contour_file_path = os.path.join(patient_day_dicoms_T1c, contours_file)
+def construct_corresponding_flair_dir(patient_day, patient_dir):
 
-rt_sequence = dicom.read_file(contour_file_path)   
-contour_names = get_roi_names(rt_sequence)
-contour_names = [name for name in contour_names if 'Day' not in name]
+    patient_day_t2f = patient_day
+    if 'CT' in patient_day:
+        patient_day_t2f = patient_day[:-len('CT')] + 'FLAIR'
+    if 'T1c' in patient_day:
+        patient_day_t2f = patient_day[:-len('T1c')] + 'FLAIR'
 
-contours_of_interest = ['T1_CTV', 'T1_GTV']
-for roi_index, contour_name in enumerate(contour_names): # TODO: note that contour that is in different time point other than day 0 seem to require coordinate registration to work, need to wait for james to give me the struct for process.
-    
-    # skip the noninterested contour annotations
-    if contour_name not in contours_of_interest:
-        continue
+    corresponding_t2f_dir = os.path.join(patient_dir, patient_day_t2f)    
+    return corresponding_t2f_dir
 
-    # get contour datasets with index idx
-    contour_datasets = get_roi_contour_ds(rt_sequence=rt_sequence, index=roi_index)
 
-    # construct mask dictionary
-    new_ReferencedSOPInstanceUIDs, new_seriesInstanceUID = get_corresponding_img_ids(contour_datasets, patient_day_dicoms_Flair)
+def construct_RTStruct_for_t2f_dicoms(params):
+    contour_names = params['contour_names']
+    rt_sequence = params['rt_sequence']
+    patient_day_dicoms_Flair = params['patient_day_dicoms_Flair']
 
-    # write the new reference id to the rt struct
-    # TODO: overwrite the rt_sequence.StudyInstanceUID if something went wrong, should be returned from get_corresponding_img_ids
-    rt_sequence.ReferencedFrameOfReferenceSequence[0].RTReferencedStudySequence[0].RTReferencedSeriesSequence[0].SeriesInstanceUID = new_seriesInstanceUID
-    for i, cd in enumerate(rt_sequence.ROIContourSequence[roi_index].ContourSequence):
-        before = cd.ContourImageSequence[0].ReferencedSOPInstanceUID
-        cd.ContourImageSequence[0].ReferencedSOPInstanceUID = new_ReferencedSOPInstanceUIDs[i]
-        # print('before: {} after: {}'.format(before, cd.ContourImageSequence[0].ReferencedSOPInstanceUID))
+    for roi_index, cname in enumerate(contour_names): # TODO: note that contour that is in different time point other than day 0 seem to require coordinate registration to work, need to wait for james to give me the struct for process.
+        print('     processing contour {}'.format(cname))
+        # get contour datasets with index idx
+        contour_datasets = get_roi_contour_ds(rt_sequence=rt_sequence, index=roi_index)
 
-rt_sequence.save_as(os.path.join(patient_day_dicoms_Flair, contours_file[:-4]+'_flair.dcm'))
-print('done')
+        # construct mask dictionary
+        new_ReferencedSOPInstanceUIDs, new_seriesInstanceUID = get_corresponding_img_ids(contour_datasets, patient_day_dicoms_Flair)
+
+        # write the new reference id to the rt struct
+        # TODO: overwrite the rt_sequence.StudyInstanceUID if something went wrong, should be returned from get_corresponding_img_ids
+        rt_sequence.ReferencedFrameOfReferenceSequence[0].RTReferencedStudySequence[0].RTReferencedSeriesSequence[0].SeriesInstanceUID = new_seriesInstanceUID
+        for i, cd in enumerate(rt_sequence.ROIContourSequence[roi_index].ContourSequence):
+            before = cd.ContourImageSequence[0].ReferencedSOPInstanceUID
+            cd.ContourImageSequence[0].ReferencedSOPInstanceUID = new_ReferencedSOPInstanceUIDs[i]
+            # print('before: {} after: {}'.format(before, cd.ContourImageSequence[0].ReferencedSOPInstanceUID))
+
+    # return a new rt_sequence that compatible with the t2f dicoms
+    return rt_sequence
+
+glio_data = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/'
+
+patients = os.listdir(glio_data)
+patients = [p for p in patients if 'GBM' in p]
+
+for patient_name in patients:
+
+    patient_contours = {} # collect the rt_struct file path for each day of the same patient
+
+    patient_dir = os.path.join(glio_data, patient_name)
+    patient_days = os.listdir(patient_dir)
+    patient_days = [d for d in patient_days if 'Day' in d and 'T1c' in d and 'Recur' not in d]
+    for patient_day in patient_days:
+        # root directory of the dicoms files within a patient day
+        patient_day_dicoms_nonT2F = os.path.join(patient_dir, patient_day) # '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/GBM108/Day0T1c'
+
+        contours_file = get_contour_file(patient_day_dicoms_nonT2F)
+        contour_file_path = os.path.join(patient_day_dicoms_nonT2F, contours_file)
+        
+        rt_sequence = dicom.read_file(contour_file_path)   
+        contour_names = get_roi_names(rt_sequence)
+        contour_names = [name for name in contour_names if 'Day' not in name]
+
+        # contruct the corresponding FLAIR directory
+        t2f_dir = construct_corresponding_flair_dir(patient_day, patient_dir)
+        print('start processing for {}'.format(t2f_dir))
+        params = {
+            'contour_names': contour_names,
+            'rt_sequence': rt_sequence,
+            'patient_day_dicoms_Flair': t2f_dir
+        }
+        new_rt_sequence = construct_RTStruct_for_t2f_dicoms(params)
+        new_rt_sequence.save_as(os.path.join(t2f_dir, contours_file[:-4]+'_flair.dcm'))
+        print('Finish processing for {}'.format(os.path.join(t2f_dir, contours_file[:-4]+'_flair.dcm')))
+
+
+# PATIENT_NAME = 'GBM149'
+# DAY_FOLDER_FLAIR_NAME = 'Day0FLAIR'
+# DAY_FOLDER_T1C_NAME = 'Day0T1c'
+
+# patient_day_dicoms_T1c = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/{}/{}'.format(PATIENT_NAME, DAY_FOLDER_T1C_NAME)
+# patient_day_dicoms_Flair = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/{}/{}'.format(PATIENT_NAME, DAY_FOLDER_FLAIR_NAME)
+# contours_file = get_contour_file(patient_day_dicoms_T1c)
+# contour_file_path = os.path.join(patient_day_dicoms_T1c, contours_file)
+
+# rt_sequence = dicom.read_file(contour_file_path)   
+# contour_names = get_roi_names(rt_sequence)
+# contour_names = [name for name in contour_names if 'Day' not in name]
+
+# for roi_index, contour_name in enumerate(contour_names): # TODO: note that contour that is in different time point other than day 0 seem to require coordinate registration to work, need to wait for james to give me the struct for process.
+
+#     # get contour datasets with index idx
+#     contour_datasets = get_roi_contour_ds(rt_sequence=rt_sequence, index=roi_index)
+
+#     # construct mask dictionary
+#     new_ReferencedSOPInstanceUIDs, new_seriesInstanceUID = get_corresponding_img_ids(contour_datasets, patient_day_dicoms_Flair)
+
+#     # write the new reference id to the rt struct
+#     # TODO: overwrite the rt_sequence.StudyInstanceUID if something went wrong, should be returned from get_corresponding_img_ids
+#     rt_sequence.ReferencedFrameOfReferenceSequence[0].RTReferencedStudySequence[0].RTReferencedSeriesSequence[0].SeriesInstanceUID = new_seriesInstanceUID
+#     for i, cd in enumerate(rt_sequence.ROIContourSequence[roi_index].ContourSequence):
+#         before = cd.ContourImageSequence[0].ReferencedSOPInstanceUID
+#         cd.ContourImageSequence[0].ReferencedSOPInstanceUID = new_ReferencedSOPInstanceUIDs[i]
+#         # print('before: {} after: {}'.format(before, cd.ContourImageSequence[0].ReferencedSOPInstanceUID))
+
+# rt_sequence.save_as(os.path.join(patient_day_dicoms_Flair, contours_file[:-4]+'_flair.dcm'))
+# print('done')
 # save the rt struct with a new name with a new location
--- glio_parser.py
+++ glio_parser.py
@@ -15,24 +15,68 @@ import pydicom as dicom
 # medical image manipulation 
 import SimpleITK as sitk
 
-PATIENT_NAME = 'GBM149'
-DAY_FOLDER_NAME = 'Day0FLAIR'
-# DAY_FOLDER_NAME = 'Day10T1c'
+glio_data = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/'
 
-patient_day_dicoms = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/{}/{}'.format(PATIENT_NAME, DAY_FOLDER_NAME)
+patients = os.listdir(glio_data)
+patients = [p for p in patients if 'GBM' in p]
 
-Dicom_reader = DicomReaderWriter(description='GBM108', arg_max=True)
-Dicom_reader.walk_through_folders(patient_day_dicoms) # need to define in order to use all_roi method
-all_rois = Dicom_reader.return_rois(print_rois=True)  # Return a list of all rois present, and print them
+for patient_name in patients:
 
-# Print the locations of all RTs with a certain ROI name, automatically lower cased
-Dicom_reader.where_is_ROI(ROIName='T1_GTV')
+    patient_contours = {} # collect the rt_struct file path for each day of the same patient
 
-Dicom_reader.set_contour_names_and_associations(['T1_GTV'])
-# Dicom_reader.get_mask()
-Dicom_reader.get_images()
-Dicom_reader.get_mask()
+    patient_dir = os.path.join(glio_data, patient_name)
+    patient_days = os.listdir(patient_dir)
+    patient_days = [d for d in patient_days if 'Recur' not in d] #NOTE: all patient day directory expects to have a struct for now
+    for patient_day in patient_days:
+        # root directory of the dicoms files within a patient day
+        patient_day_dicoms = os.path.join(patient_dir, patient_day) # '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/GBM108/Day0T1c'
 
-sitk.WriteImage(Dicom_reader.dicom_handle, '/Users/maxxyouu/Desktop/IAMLAB/Contour_outputs_test/{}_{}.nii.gz'.format(PATIENT_NAME, DAY_FOLDER_NAME))
-sitk.WriteImage(Dicom_reader.annotation_handle, '/Users/maxxyouu/Desktop/IAMLAB/Contour_outputs_test/{}_{}_mask.nii.gz'.format(PATIENT_NAME, DAY_FOLDER_NAME))
+        Dicom_reader = DicomReaderWriter(description=patient_name, arg_max=True)
+        Dicom_reader.walk_through_folders(patient_day_dicoms) # need to define in order to use all_roi method
+        all_rois = Dicom_reader.return_rois(print_rois=True)  # Return a list of all rois present, and print them
+
+        # filter out the contours that do not belong to this
+        all_rois = [roi for roi in all_rois if ('day' not in roi) or 'day0' in roi]
+
+        # convert the format of the dicom image to nii  TODO: move the files to the correct directory
+        sitk.WriteImage(Dicom_reader.dicom_handle, '/Users/maxxyouu/Desktop/IAMLAB/Contour_outputs_test/{}_{}.nii.gz'.format(patient_name, patient_day))
+        for roi in all_rois:
+            print('processing contour {} for patient {} on day {}'.format(roi, patient_name, patient_day))
+            ROI_NAME = roi
+            # Print the locations of all RTs with a certain ROI name, automatically lower cased
+            Dicom_reader.where_is_ROI(ROIName=ROI_NAME)
+            Dicom_reader.set_contour_names_and_associations([ROI_NAME])
+            # Dicom_reader.get_images()
+            # Dicom_reader.get_mask()
+            Dicom_reader.get_images_and_mask()
+            # TODO: move the files to the correct directory
+            sitk.WriteImage(Dicom_reader.annotation_handle, '/Users/maxxyouu/Desktop/IAMLAB/Contour_outputs_test/{}_{}_mask_{}.nii.gz'.format(PATIENT_NAME, DAY_FOLDER_NAME, ROI_NAME))
 print('done')
+
+# PATIENT_NAME = 'GBM108'
+# DAY_FOLDER_NAME = 'Day0FLAIR'
+# # DAY_FOLDER_NAME = 'Day10T1c'
+# # ROI_NAME = 'brainstem'
+
+# patient_day_dicoms = '/Users/maxxyouu/Desktop/IAMLAB/GBMforAprilV2-origin/{}/{}'.format(PATIENT_NAME, DAY_FOLDER_NAME)
+
+# Dicom_reader = DicomReaderWriter(description=PATIENT_NAME, arg_max=True)
+# Dicom_reader.walk_through_folders(patient_day_dicoms) # need to define in order to use all_roi method
+# all_rois = Dicom_reader.return_rois(print_rois=True)  # Return a list of all rois present, and print them
+
+# # filter out the contours that do not belong to this
+# all_rois = [roi for roi in all_rois if ('day' not in roi) or 'day0' in roi]
+
+# sitk.WriteImage(Dicom_reader.dicom_handle, '/Users/maxxyouu/Desktop/IAMLAB/Contour_outputs_test/{}_{}.nii.gz'.format(PATIENT_NAME, DAY_FOLDER_NAME))
+# for roi in all_rois:
+#     print('processing contour {}'.format(roi))
+#     ROI_NAME = roi
+#     # Print the locations of all RTs with a certain ROI name, automatically lower cased
+#     Dicom_reader.where_is_ROI(ROIName=ROI_NAME)
+#     Dicom_reader.set_contour_names_and_associations([ROI_NAME])
+#     # Dicom_reader.get_images()
+#     # Dicom_reader.get_mask()
+#     Dicom_reader.get_images_and_mask()
+#     sitk.WriteImage(Dicom_reader.annotation_handle, '/Users/maxxyouu/Desktop/IAMLAB/Contour_outputs_test/{}_{}_mask_{}.nii.gz'.format(PATIENT_NAME, DAY_FOLDER_NAME, ROI_NAME))
+
+# print('done')
